f7255d4fc9a3652bd3557f8dcdbf4195e3ede72d
zxf724 <565737064@qq.com>
2018-08-21 21:42:11 +0800

    user linux c io

--- vertebra.c
+++ vertebra.c
@@ -4,6 +4,7 @@
  *NAME:WENZONG
  *DATE:2018/08/20
  * ***************************/
+/***********************first step
 #include <stdio.h>  
 #include <stdlib.h>  
 #include <string.h>  
@@ -17,11 +18,11 @@
   
 int main()  
 {  
- 	 char buf[MAX_LINE];  /*缓冲区*/  
-	 FILE *fp;            /*文件指针*/  
-	 int len;             /*行字符个数*/  
+ 	 char buf[MAX_LINE];  //缓冲区 
+	 FILE *fp;            //文件指针  
+	 int len;             //行字符个数  
   
-	 if((fp = fopen("novel.txt","r")) == NULL)  
+	 if((fp = fopen("ADeadlySecret.txt","r")) == NULL)  
 	 {  
 		 perror("fail to read");  
 		 exit (1) ;  
@@ -30,9 +31,33 @@ int main()
 	 while(fgets(buf,MAX_LINE,fp) != NULL)  
 	 {  
 		 len = strlen(buf);  
-		 buf[len-1] = '\0';  /*去掉换行符*/  
+		 buf[len-1] = '\0';  //去掉换行符
 	//	 printf("%s %d \n",buf,len - 1);  
 		 printf("%s \n", buf);
 	 }  
   	return 0;  
-}  
+}
+*/
+
+/***linux  file io***/
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define NOVEL  novel.txt
+
+int main()
+{
+	int  fd_noverl;
+
+	if(fd_novel = open(NOVEL,O_RDONLY) > 0)
+		printf("open novel successed");
+
+
+
+
+}
